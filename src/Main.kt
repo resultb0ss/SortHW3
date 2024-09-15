615ed2ff95d79534d6570eb0535010b2639afbf4
Admin <reteamboss@gmail.com>
2024-09-15 12:55:58 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,25 @@
 fun main() {
-    println("Hello World!")
+    var array  = arrayOf(
+        arrayOf(1,2,3,45),
+        arrayOf(445,35,243,78),
+        arrayOf(564,345,24,65),
+    )
+
+    var count = 0
+
+    for (j in array.indices){
+        for (i in array.indices)
+            if ((array[j][i + 1] - array[j][i]) * (array[j][i + 2] - array[j] [i + 1]) >= 0) {
+                println("${array[i]}Пилообразнный")
+                count += 1
+                break
+            } else {
+                println("${array[i]} не пилообразный")
+            }
+
+        println(count)
+    }
+
+
+
 }
